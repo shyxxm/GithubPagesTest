58377f6f74bad349ded43b5c3d99bc0b0f240638
Shyam Prasad <48471694+shyxxm@users.noreply.github.com>
2023-07-25 11:19:22 +0100

    testing

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -5,6 +5,6 @@ export const aboutMeData = [
 	},
 	{
 		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+		bio: 'Jeff abo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
 	},
 ];
