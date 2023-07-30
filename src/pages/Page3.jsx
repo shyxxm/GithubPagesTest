440becee336e9f472633a348e77b6b991c985bda
miscting <jefflee.jh@gmail.com>
2023-07-30 19:30:26 +0100

    Carousell for figma image
    npm install react-multi-carousel --save

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -1,6 +1,10 @@
 import { motion } from 'framer-motion';
 import ContactDetails from '../components/contact/ContactDetails';
 import ContactForm from '../components/contact/ContactForm';
+import React, { useState } from 'react'; 
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 
 // const Contact = () => {
 // 	return (
@@ -21,6 +25,20 @@ import ContactForm from '../components/contact/ContactForm';
 // };
 
 const Prototyping = () => {
+	// array of image file names for initial
+	const longImages = Array.from({ length: 34 }, (_, index) => `Flow_2.${index + 1}.png`);
+   
+	// array of image file names for final
+	const shortImages = Array.from({ length: 26 }, (_, index) => `Flow_3.${index + 1}.png`);
+
+	const responsive = {
+		desktop: {
+		  breakpoint: { max: 3000, min: 1024 },
+		  items: 5,
+		  slidesToSlide: 3, // Show 3 images at a time
+		}
+	  };
+
 	return (
 	  <>
 		{/* Section - Hi-Fi Prototype #Initial */}
@@ -82,6 +100,41 @@ const Prototyping = () => {
         	</ul>
 		</section>
 
+   {/* Carousel for Figma_Flow_Long */}
+   <section style={{ marginLeft: '10%', marginRight: '10%', padding: '1rem 2rem' }}>
+        <h2 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Figma_Flow_Long</h2>
+        <Carousel
+          responsive={responsive} // Use the responsive configuration
+          swipeable={true} // Enable swipe gestures
+          draggable={true} // Enable drag and swipe
+          showDots={false} // Hide the navigation dots
+          infinite={true} // Enable infinite scrolling
+          containerClass="carousel-container" // Optional class for the carousel container
+          itemClass="carousel-item" // Optional class for each carousel item
+        >
+          {longImages.map((imageName, index) => (
+            <div key={index} className="carousel-image-wrapper">
+              <img
+                src={require(`../images/Figma_Flow_Long/${imageName}`).default}
+                alt={`Figma_Flow_Long_${index}`}
+                style={{ maxWidth: '80%', height: 'auto' }}
+              />
+            </div>
+          ))}
+        </Carousel>
+      </section>
+
+      {/* Carousel for Figma_Flow_Short */}
+      {/* <section style={{ marginLeft: '10%', marginRight: '10%', padding: '1rem 2rem' }}>
+        <h2 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Figma_Flow_Short</h2>
+        <Carousel showArrows={true} emulateTouch={true}>
+          {shortImages.map((imageName, index) => (
+            <div key={index}>
+              <img src={require(`../images/Figma_Flow_Short/${imageName}`).default} alt={`Figma_Flow_Short_${index}`} style={{ maxWidth: '100%', height: 'auto' }} />
+            </div>
+          ))}
+        </Carousel>
+      </section> */}
 	  </>
 	);
   };
