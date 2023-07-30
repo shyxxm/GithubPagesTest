0315d0e2e59524c19d68f230a5d5089ada10bd8c
miscting <jefflee.jh@gmail.com>
2023-07-30 18:59:08 +0100

    Edited Naming of prototypes

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -23,10 +23,10 @@ import ContactForm from '../components/contact/ContactForm';
 const Prototyping = () => {
 	return (
 	  <>
-		{/* Section - Hi-Fi Prototype #1 */}
+		{/* Section - Hi-Fi Prototype #Initial */}
 		<section style={{ marginLeft: '10%', marginRight: '10%', padding: '1rem 2rem' }}>
         <h2 style={{ fontSize: '3rem', marginBottom: '1rem' }}>Hi-Fi Prototypes</h2>
-        <h2 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Hi-Fi Prototype #1</h2>
+        <h2 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Hi-Fi Prototype #Initial</h2>
         {/* Sub-Headers */}
         <ul style={{ fontSize: '1.2rem' }}>
           <li style={{ textDecoration: 'underline'}}>App Design</li>
@@ -41,26 +41,6 @@ const Prototyping = () => {
           <p style={{marginBottom: '1rem'}}>This is the placeholder text for Design Justification.</p>
         </ul>
       </section>
-		
-		<hr style={{ margin: '2rem 10%', border: '1px solid' }} className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light"/>
-
-		{/* Section - Hi-Fi Prototype #2 */}
-		<section style={{ marginLeft: '10%', marginRight: '10%', padding: '1rem 2rem' }}>		
-		  	<h2 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Hi-Fi Prototype #2 </h2>
-			{/* Sub-Headers */}
-			<ul style={{ fontSize: '1.2rem' }}>
-				<li style={{ textDecoration: 'underline'}}>App Design</li>
-				<p style={{marginBottom: '1rem'}}>This is the placeholder text for App Design.</p>
-				<li style={{ textDecoration: 'underline' }}>Color Theme</li>
-				<p style={{marginBottom: '1rem'}}>This is the placeholder text for Color Theme.</p>
-				<li style={{ textDecoration: 'underline' }}>Assets</li>
-				<p style={{marginBottom: '1rem'}}>This is the placeholder text for Assets.</p>
-				<li style={{ textDecoration: 'underline' }}>Cartoon Style</li>
-				<p style={{marginBottom: '1rem'}}>This is the placeholder text for Cartoon Style.</p>
-				<li style={{ textDecoration: 'underline' }}>Design Justification</li>
-				<p style={{marginBottom: '1rem'}}>This is the placeholder text for Design Justification.</p>
-        	</ul>
-		</section>
 
 		<hr style={{ margin: '2rem 10%', border: '1px solid' }} className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light"/>		
 
